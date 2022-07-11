cc14177b6644ceca3fee2412679de108b85afd37
alejandrosantanabonilla <62341142+alejandrosantanabonilla@users.noreply.github.com>
2022-07-11 11:58:29 +0100

    Create job.sh

---
+++ CP2K_tutorial/MD/B3LYP/job.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -l
+#$ -l h_rt=5:00:00
+#$ -l mem=4.5G
+#$ -pe mpi 80
+#$ -N cp2k_b3lyp_optimization
+#$ -A KCL_Admin_rse
+#$ -P Gold
+#$ -cwd 
+
+module purge
+module load gerun
+module load gcc-libs
+module load compilers/gnu/4.9.2
+module load mpi/openmpi/3.1.4/gnu-4.9.2
+module load openblas/0.3.7-openmp/gnu-4.9.2
+module load cp2k/7.1/ompi/gnu-4.9.2
+
+export OMP_NUM_THREADS=1
+gerun cp2k.popt -inp md.inp > output.out 
