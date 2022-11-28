6fc6c4babdbd4be07bb5f150345f3790661965f4
Alejandro Santana-Bonilla <62341142+alejandrosantanabonilla@users.noreply.github.com>
2022-11-28 12:26:32 +0000

    Create job_normal.sh

---
+++ vayesta/submission/job_normal.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -l                                                                                                                                         
+#$ -l h_rt=48:00:00                                                                                                                                    
+#$ -l mem=4.5G                                                                                                                                          
+#$ -pe mpi 40                                                                                                                                          
+#$ -N mpi_vayesta
+#$ -A KCL_YOUR_GROUP                                                                                                                                                                                                                                                                       
+#$ -P Gold                                                                                                                                             
+#$ -cwd
+
+module purge
+module load beta-modules
+module load gcc-libs/10.2.0
+module load python/3.9.6
+module load openblas/0.3.13-openmp/gnu-10.2.0
+module load compilers/gnu/10.2.0
+module load mpi/openmpi/4.0.5/gnu-10.2.0
+module load gerun
+
+source $HOME/work/pol/bin/activate
+
+export PATH=$HOME/work/pol/bin:$PATH
+export PYTHONPATH=$HOME/work/pol/lib/python3.9/site-packages:$PYTHONPATH
+
+export PYTHONPATH=$PYTHONPATH:$HOME/vayesta_installation/pyscf
+export PYTHONPATH=$PYTHONPATH:$HOME/vayesta_installation/Vayesta
+
+export OMP_NUM_THREADS=1
+gerun python 90-mpi.py > output.out
