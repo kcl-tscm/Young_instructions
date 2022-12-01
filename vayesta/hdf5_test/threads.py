d51bc1dcf9f56eb5c52a99a4c6112ab912a0f103
Alejandro Santana-Bonilla <62341142+alejandrosantanabonilla@users.noreply.github.com>
2022-12-01 13:23:58 +0000

    Create threads.py

---
+++ vayesta/hdf5_test/threads.py
@@ -0,0 +1,11 @@
+from mpi4py import MPI
+import h5py
+
+rank = MPI.COMM_WORLD.rank  # The process ID which is an integer                                                                                                                  
+
+f = h5py.File('parallel_test.hdf5', 'w', driver='mpio', comm=MPI.COMM_WORLD)
+
+dset = f.create_dataset('test', (80,), dtype='i')
+dset[rank] = rank
+
+f.close()
